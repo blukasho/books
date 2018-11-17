1596e6619acb3513b7395eabcb6a42672fe0091c
blukasho <bodik1w@gmail.com>
2018-11-17 23:24:59 +0200

    new 1.6/bar_chart_symbols.c, mod 1.6/bar_chart_word_length.c, mod 1.6/bar_chart_word_length_v2.c

--- k&r/1.6/bar_chart_symbols.c
+++ k&r/1.6/bar_chart_symbols.c
@@ -1,53 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bar_chart_word_length.c                            :+:      :+:    :+:   */
+/*   bar_chart_symbols.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/17 17:32:02 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/17 23:19:18 by blukasho         ###   ########.fr       */
+/*   Created: 2018/11/17 22:24:28 by blukasho          #+#    #+#             */
+/*   Updated: 2018/11/17 23:11:08 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-#define MAX_LEN 50
-#define IN 1
-#define OUT 0
+#define MIN 33
+#define MAX 127
 
 int		main(void)
 {
-	int wl[MAX_LEN];
-	int i, c, l, state;
+	int ch[MAX - MIN];
+	int c, i;
 
-	state = l = OUT;
-	for (i = 0; i < MAX_LEN; ++i)
-		wl[i] = 0;
+	for (i = 0; i < (MAX - MIN); ++i)
+		ch[i] = 0;
 	while ((c = getchar()) != EOF)
-	{
-		if (c == ' ' || c == '\n' || c == '\t')
-		{
-			if (l <= MAX_LEN && state == IN)
-				++wl[l - 1];
-			state = l = OUT;
-		}
-		else
-		{
-			state = IN;
-			++l;
-		}
-	}
-	for (i = 1; i <= MAX_LEN; ++i)
-		printf("%3d", i);
+		if (c >= MIN && c < MAX)
+			++ch[c - MIN];
+	for (i = 0; i < 20; ++i)
+		printf("\\|/");
 	printf("\n");
-	for (state = 0; state < MAX_LEN; ++state)
+	for (c = MIN; c < MAX; ++c)
 	{
-		for (i = 0; i < MAX_LEN; ++i)
-			if (wl[i] > 0 && wl[i]--)
-				printf("  *");
-			else
-				printf("   ");
+		printf("%c ", c);
+		for (i = c - MIN; ch[i] > 0; --ch[i])
+			printf("*");
 		printf("\n");
 	}
 	return (0);
